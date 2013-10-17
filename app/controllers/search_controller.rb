cea5636399dac156d79c990b63e946f0807c7763
Shivam Daryanani <daryananis@gmail.com>
2013-10-18 02:23:09 +0400

    Vimeo search action

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -13,4 +13,15 @@ class SearchController < ApplicationController
     end
   end
 
+  def vimeo 
+    vimeo = Vimeo::Advanced::Video.new(ENV["VIMEO_CONSUMER_KEY"],ENV["VIMEO_CONSUMER_SECRET"],token: ENV["VIMEO_ACCESS_TOKEN"], secret: ENV["VIMEO_ACCESS_SECRET"])
+    response = vimeo.search(params[:query], { :page => "1", :per_page => "25", :full_response => "1"}) if params[:query]
+    if response && response["videos"]["video"]
+      videos = response["videos"]["video"].map { |video| format_vimeo_response(video) }
+      render json: videos, status: 200, query: params[:query]
+    else
+      render json: params[:query], status: 404
+    end
+  end
+
 end
