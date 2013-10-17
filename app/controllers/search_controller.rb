51b57af6ff4f9b2b4918fcf307ef8520139d6976
Shivam Daryanani <daryananis@gmail.com>
2013-10-18 03:02:21 +0400

    Cleaned up controller and moved search methods to the helper for cleaner code and also to be able to stub these methods for future tests

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,8 +3,7 @@ class SearchController < ApplicationController
   include SearchHelper
 
   def youtube
-    client = YouTubeIt::Client.new
-    response =  client.videos_by(:query => params[:query]) if params[:query].present?
+    response = get_youtube_videos(params[:query])
     if response && response.videos.present?
       videos = response.videos.map{ |video| format_youtube_response(video) }
       render :json => videos, :status => 200, :query => params[:query]
@@ -14,8 +13,7 @@ class SearchController < ApplicationController
   end
 
   def vimeo
-    vimeo = Vimeo::Advanced::Video.new(ENV["VIMEO_CONSUMER_KEY"],ENV["VIMEO_CONSUMER_SECRET"],token: ENV["VIMEO_ACCESS_TOKEN"], secret: ENV["VIMEO_ACCESS_SECRET"])
-    response = vimeo.search(params[:query], { :page => "1", :per_page => "25", :full_response => "1"}) if params[:query].present?
+    response = get_vimeo_videos(params[:query])
     if response && response["videos"]["video"].present?
       videos = response["videos"]["video"].map { |video| format_vimeo_response(video) }
       render json: videos, status: 200, query: params[:query]
--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -2,18 +2,28 @@ module SearchHelper
 	include ActionView::Helpers::DateHelper
 	include ActionView::Helpers::NumberHelper
 
+  def get_youtube_videos(query)
+    client = YouTubeIt::Client.new
+    response =  client.videos_by(:query => query) if query.present?
+  end
+
   def format_youtube_response(video)
     {
-		  image: video.thumbnails[1].url,
-		  duration: Time.at(video.duration).utc.strftime("%H:%M:%S"),
-		  title: video.title,
-		  user_name: video.author.name,
-		  date: time_ago_in_words(video.published_at),
-		  view_count: number_with_delimiter(video.view_count, :delimiter => ','),
-		  description: video.description,
-		  id: video.video_id.match(/video:(.+)/)[1],
-		  url: video.player_url
+      image: video.thumbnails[1].url,
+      duration: Time.at(video.duration).utc.strftime("%H:%M:%S"),
+      title: video.title,
+      user_name: video.author.name,
+      date: time_ago_in_words(video.published_at),
+      view_count: number_with_delimiter(video.view_count, :delimiter => ','),
+      description: video.description,
+      id: video.video_id.match(/video:(.+)/)[1],
+      url: video.player_url
      }
+  end
+
+  def get_vimeo_videos(query)
+    vimeo = Vimeo::Advanced::Video.new(ENV["VIMEO_CONSUMER_KEY"],ENV["VIMEO_CONSUMER_SECRET"],token: ENV["VIMEO_ACCESS_TOKEN"], secret: ENV["VIMEO_ACCESS_SECRET"])
+    response = vimeo.search(query, { :page => "1", :per_page => "25", :full_response => "1"}) if query.present?
   end
 
   def format_vimeo_response(video)
