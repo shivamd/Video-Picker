1cb2bd7735e361d507e9aa09c89acac4206a5bb6
Stephane Thomas <thomas.stephn@gmail.com>
2013-10-17 11:29:22 -0400

    initial rails 4 app commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Videopicker::Application.config.secret_key_base = 'cd24cc61b2f663f925f05369c0a53dd48226c2ac892e838b6c9645f30f16f2263112e212a8d34e861b5494eac302a12e7d091c4b5b1690d2398888e8f21af690'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Videopicker::Application.config.session_store :cookie_store, key: '_videopicker_session'
