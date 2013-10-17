da7892db701647fd57e82b3219c69410fc52f058
Shivam Daryanani <daryananis@gmail.com>
2013-10-18 01:08:32 +0400

    Minimal tests for youtube method and adjustments on the method

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,11 +1,11 @@
 class SearchController < ApplicationController
- 
+
   include SearchHelper
 
   def youtube
     client = YouTubeIt::Client.new
-    response =  client.videos_by(:query => params[:query])
-    if response.videos.present?
+    response =  client.videos_by(:query => params[:query]) if params[:query].present?
+    if response && response.videos.present?
       videos = response.videos.map{ |video| format_youtube_response(video) }
       render :json => videos, :status => 200, :query => params[:query]
     else
---
+++ spec/controllers/search_controller_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+describe SearchController do
+
+	describe "#youtube" do
+		it "should return a 200" do
+			get :youtube,  :query => 'test'
+			expect(response.status).to eq(200)
+		end
+
+		it "should not make a search if query is blank" do
+			get :youtube,  :query => ''
+			expect(response.status).to eq(404)
+		end
+
+		it "should 404 if there are no results" do
+			get :youtube,  :query => 'aosuhfsaouf'
+			expect(response.status).to eq(404)
+		end
+
+		it "should return JSON data" do
+			get :youtube,  :query => 'test'
+			response.header['Content-Type'].should include 'application/json'
+		end
+	end
+end
