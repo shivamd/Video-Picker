37e27d1cb5f07095c880cff5acaee1b721bc4ff9
Shivam Daryanani <daryananis@gmail.com>
2013-10-17 21:04:47 +0400

    Youtube search action

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,2 +1,16 @@
 class SearchController < ApplicationController
+ 
+  include SearchHelper
+
+  def youtube
+    client = YouTubeIt::Client.new
+    response =  client.videos_by(:query => params[:query])
+    if response.videos.present?
+      videos = response.videos.map{ |video| format_youtube_response(video) }
+      render :json => videos, :status => 200, :query => params[:query]
+    else
+      render :json => params[:query] ,:status => 404
+    end
+  end
+
 end
