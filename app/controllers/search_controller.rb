cf0c7242309c5ef46e7b75ded026395d5ae17ddd
Shivam Daryanani <daryananis@gmail.com>
2013-10-18 02:29:49 +0400

    Tests for controllers and adjustments on the action itself in order to pass the tests(VIMEO)

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -13,10 +13,10 @@ class SearchController < ApplicationController
     end
   end
 
-  def vimeo 
+  def vimeo
     vimeo = Vimeo::Advanced::Video.new(ENV["VIMEO_CONSUMER_KEY"],ENV["VIMEO_CONSUMER_SECRET"],token: ENV["VIMEO_ACCESS_TOKEN"], secret: ENV["VIMEO_ACCESS_SECRET"])
-    response = vimeo.search(params[:query], { :page => "1", :per_page => "25", :full_response => "1"}) if params[:query]
-    if response && response["videos"]["video"]
+    response = vimeo.search(params[:query], { :page => "1", :per_page => "25", :full_response => "1"}) if params[:query].present?
+    if response && response["videos"]["video"].present?
       videos = response["videos"]["video"].map { |video| format_vimeo_response(video) }
       render json: videos, status: 200, query: params[:query]
     else
--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -22,4 +22,27 @@ describe SearchController do
 			response.header['Content-Type'].should include 'application/json'
 		end
 	end
+
+	describe "#vimeo" do
+		it "should return a 200" do
+			get :vimeo,  :query => 'test'
+			expect(response.status).to eq(200)
+		end
+
+		it "should not make a search if query is blank" do
+			get :vimeo,  :query => ''
+			expect(response.status).to eq(404)
+		end
+
+		it "should 404 if there are no results" do
+			get :vimeo,  :query => 'uaohsuhsdf'
+			expect(response.status).to eq(404)
+		end
+
+		it "should return JSON data" do
+			get :vimeo,  :query => 'test'
+			response.header['Content-Type'].should include 'application/json'
+		end
+	end
+
 end
