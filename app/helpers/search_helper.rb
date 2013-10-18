dd78a51bc171588758ba815fb715888c1fa803d8
Shivam Daryanani <daryananis@gmail.com>
2013-10-18 18:56:16 +0400

    Helper methods for dailymotion search

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -40,4 +40,25 @@ module SearchHelper
     }
   end
 
+  def get_dailymotion_videos(query)
+    if query.present?
+      response = open("https://api.dailymotion.com/videos?search=#{query}&limit=25&fields=created_time,title,id,description,duration,thumbnail_240_url,owner,url,views_total").read
+      JSON.parse(response)
+    end
+  end
+
+  def format_dailymotion_response(video)
+    {
+      image: video["thumbnail_240_url"],
+      duration: video["duration"],
+      title: video["title"],
+      user_name: video["owner"],
+      date: time_ago_in_words(Time.at(video["created_time"]).utc.to_datetime), #timestamp to time 
+      view_count: video["views_total"],
+      description: video["description"],
+      id: video["id"],
+      url: video["url"]
+    }
+  end
+
 end
