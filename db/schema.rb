af70ceda1654552694aa6f42ae933c8546f1a14f
Shivam Daryanani <daryananis@gmail.com>
2013-10-29 19:43:10 +0400

    Schema&Migration for applications table

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131029152944) do
+ActiveRecord::Schema.define(version: 20131029154218) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "applications", force: true do |t|
+    t.integer "user_id"
+    t.string  "name"
+    t.string  "access_token"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
