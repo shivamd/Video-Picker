78d56ef9ab29187aa98cddea0f5e414cba63b789
Shivam Daryanani <daryananis@gmail.com>
2013-10-29 19:52:21 +0400

    Access token generation on application creation

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,5 +1,13 @@
 class Application < ActiveRecord::Base
-  attr_accessible :name
+  before_create :generate_access_token
 
   belongs_to :user
+
+  private
+
+  def generate_access_token
+    begin 
+      self.access_token = SecureRandom.hex
+    end while self.class.exists?(access_token: access_token)
+  end
 end
