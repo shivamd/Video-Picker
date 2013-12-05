43b8748b85459e5041061fba3c27e28b9ffd785e
Shivam Daryanani <daryananis@gmail.com>
2013-12-05 20:38:50 +0400

    Removed env keys setting from application.rb

--- config/application.rb
+++ config/application.rb
@@ -9,17 +9,6 @@ Bundler.require(:default, Rails.env)
 module Videopicker
   class Application < Rails::Application
 
-    api_keys = YAML.load_file(Rails.root.join('config','keys.yml'))
-    ENV["VIMEO_CONSUMER_KEY"] = api_keys["VIMEO_CONSUMER_KEY"]
-    ENV["VIMEO_CONSUMER_SECRET"] = api_keys["VIMEO_CONSUMER_SECRET"]
-    ENV["VIMEO_ACCESS_TOKEN"] = api_keys["VIMEO_ACCESS_TOKEN"]
-    ENV["VIMEO_ACCESS_SECRET"] = api_keys["VIMEO_ACCESS_SECRET"]
-    ENV["TWITTER_CONSUMER_KEY"] = api_keys["TWITTER_CONSUMER_KEY"]
-    ENV["TWITTER_CONSUMER_SECRET"] = api_keys["TWITTER_CONSUMER_SECRET"]
-    ENV["TWITTER_ACCESS_TOKEN"] = api_keys["TWITTER_ACCESS_TOKEN"]
-    ENV["TWITTER_ACCESS_SECRET"] = api_keys["TWITTER_ACCESS_SECRET"]
-    ENV["INSTAGRAM_CLIENT_ID"] = api_keys["INSTAGRAM_CLIENT_ID"]
-    ENV["INSTAGRAM_SECRET"] = api_keys["INSTAGRAM_SECRET"]
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
