676b6a89daa55a15cdcc5b25e0167184f115a82a
Shivam Daryanani <daryananis@gmail.com>
2013-12-07 11:45:43 +0400

    Cleanup

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,8 +4,4 @@ class RegistrationsController < Devise::RegistrationsController
   def after_sign_up_path_for(resource)
     new_application_path
   end
-
-  def after_sign_in_path_for(resource)
-    new_application_path
-  end
 end
